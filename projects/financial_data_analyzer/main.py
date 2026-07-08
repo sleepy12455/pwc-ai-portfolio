43fd6b7535c97a6bfb82c9ef6b7a46beef09f3ec
노승욱 <tmddnrwkdrk@gmail.com>
2026-07-08 15:26:08 +0900

    Add sales and profit analysis functions

--- projects/financial_data_analyzer/main.py
+++ projects/financial_data_analyzer/main.py
@@ -1,12 +1,50 @@
 import csv
 
-total_sales = 0
+def calculate_total_sales(filename):
+    total_sales = 0
 
-with open("sample_data.csv", "r", encoding="utf-8") as file:
-    reader = csv.DictReader(file)
+    with open(filename, "r", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+        
+        for row in reader:
+            total_sales += int(row["매출"])
 
-    for row in reader:  
-        total_sales += int(row["매출"])
+        return total_sales
 
-print("총매출 : ", total_sales)
+def calculate_average_profit(filename):
+    total_profit = 0
+    count = 0
 
+    with open(filename, "r", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+
+        for row in reader:
+            total_profit += int(row["영업이익"])
+            count += 1
+        
+        return total_profit / count
+    
+print("=============================")
+print("Financial Data Analyzer")
+print("=============================")
+
+print ("총매출: ", calculate_total_sales("sample_data.csv"))
+print ("평균 영업이익: ", calculate_average_profit("sample_data.csv"))
+
+def find_best_company(filename):
+    best_company = ""
+    highest_sales = 0
+
+    with open(filename, "r", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+
+        for row in reader:
+            sales = int(row["매출"])
+
+            if sales > highest_sales:
+                highest_sales = sales
+                best_company = row["회사"]
+
+            return best_company
+
+print("최고 매출 기업:", find_best_company("sample_data.csv"))
